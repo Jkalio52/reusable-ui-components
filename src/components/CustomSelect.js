d0bedb84b9b204b19b11fcebcdd17cb803bd411d
Jay Kalio <jerrykalio@gmail.com>
2021-06-30 01:52:40 -0500

    Add comment to custom select component

--- src/components/CustomSelect.js
+++ src/components/CustomSelect.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 
 // To build the option tags, you looped through the data set via props to construct it before rendering it as part of the select tag.
+// The state of the tag (the currently selected option) is stored locally and updated and sent back as an output when it changes via the local function handleChange()
 function CustomSelect(props) {
   const [data] = useState(props.data);
   const [selectedData, updateSelectedData] = useState("");
