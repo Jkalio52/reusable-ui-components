67179d23539b00a4f5f71fab3b2ca6add7de5580
Jay Kalio <jerrykalio@gmail.com>
2021-06-30 01:41:03 -0500

    Add comment to button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-
+// Reusable button to display different color variants or sizes everywhere it is used in your application.
 function Button(props) {
   const [size] = useState(props.size);
   const [variant] = useState(props.variant);
