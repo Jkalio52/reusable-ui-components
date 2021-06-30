df4516a12244be10fc08405e70bec52c5e3c16db
Jay Kalio <jerrykalio@gmail.com>
2021-06-30 01:58:28 -0500

    Add more comment to the buttom component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 
 // Reusable button to display different color variants or sizes everywhere it is used in your application.
+// The button receives three properties via props. The variant (used to determine the button color), the size (lg, xs, sm) to determine the size of the button. You display the button content dynamically using React’s built-in children property (props.children).
 function Button(props) {
   const [size] = useState(props.size);
   const [variant] = useState(props.variant);
