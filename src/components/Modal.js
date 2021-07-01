afe7fd05de11157552de710eadfe37c203f198b1
Jay Kalio <jerrykalio@gmail.com>
2021-07-01 00:55:32 -0500

    Modify modal component.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,1 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+
 // A modal component is suitable for sending alerts in your application.
+function Modal(props) {
+  const [message] = useState(props.message);
+  const [show, setShow] = useState(props.show);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 3000);
+  });
+
+  return (
+    <div className={show ? "modal display-block" : "modal display-none"}>
+      <section className="modal-main">{message}</section>
+    </div>
+  );
+}
+
+export default Modal;
