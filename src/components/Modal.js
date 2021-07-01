e45b86fdf16cee32a63cd7e0370b02dad6f6fa8f
Jay Kalio <jerrykalio@gmail.com>
2021-07-01 00:59:56 -0500

    Add comment to modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from "react";
 
 
 // A modal component is suitable for sending alerts in your application.
+/*
+This modal component does two things:
+
+    1. It receives a boolean value that determines if it pops up or not
+    2. It also receives the message to be displayed when it pops up
+
+To close the modal, set the show state to false. You can do that by calling a setTimeout() function in the useEffect() hook after a few seconds.
+*/
 function Modal(props) {
   const [message] = useState(props.message);
   const [show, setShow] = useState(props.show);
