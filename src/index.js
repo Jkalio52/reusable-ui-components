e619990a14fc34933b3e98338dd0d07a8a4af36f
Jay Kalio <jerrykalio@gmail.com>
2021-07-08 00:58:50 -0500

    Import button component file in index js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import FormInput from "../components/FormInput.js";
 import CustomSelect from "../components/CustomSelect.js";
 import ToggleSwitch from "../components/ToggleSwitch.js";
+import Button from "../components/Button.js";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
